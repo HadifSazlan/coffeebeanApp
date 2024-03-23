3425baeece18ecfae3a86d26c117b22479c5cda2
Afiq Sazlan <afiqsazlan@gmail.com>
2024-03-23 15:08:17 +0800

    Displayed list of beans on Home

---
+++ src/app/components/BeanCard.tsx
@@ -0,0 +1,32 @@
+import { CoffeeBean } from "@/types/CoffeeBean";
+import React from "react";
+
+interface BeanCardProps {
+    bean: CoffeeBean;
+}
+
+export const BeanCard: React.FC<BeanCardProps> = ({ bean }) => {
+    const beanDetails = (
+        <ul>
+            <li>Origin: {bean.origin}</li>
+            <li>Flavor: {bean.flavor}</li>
+            <li>Process: {bean.process}</li>
+            <li>Altitude: {bean.altitude}</li>
+        </ul>
+    );
+
+    return (
+        <div className="border py-4  space-y-2 rounded-lg">
+            <div className="px-4">
+                <p className="text-xl font-bold tracking-wide">{bean.name}</p>
+                <div className="text-sm space-y-2 mt-2">{beanDetails}</div>
+            </div>
+            <div className="border-t px-4">
+                <p className="text-xs  divide-x-2 pt-2">
+                    {bean.roasterName && <span className="pr-1">Roasted by {bean.roasterName} </span>}
+                    {bean.supplierName && <span className="pl-1">Supplied by {bean.supplierName}</span>}
+                </p>
+            </div>
+        </div>
+    );
+};
---
+++ src/types/CoffeeBean.ts
@@ -0,0 +1,12 @@
+export type CoffeeBean = {
+    id: number
+    name: string
+    origin: string
+    flavor: string
+    process: string
+    altitude: string
+    roasterId?: number
+    roasterName?:string
+    supplierId?: number
+    supplierName?:string
+}
